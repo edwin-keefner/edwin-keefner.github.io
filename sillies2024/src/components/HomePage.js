94211daf5527e0aed16f6e1d538617f4f9a56c4e
edwin-keefner <113655073+edwin-keefner@users.noreply.github.com>
2024-09-17 21:53:05 -0400

    test

--- sillies2024/src/components/HomePage.js
+++ sillies2024/src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Our Website</h1>
+      <h1>HI GUYS AND GALS</h1>
       <p>Some information about the website.</p>
     </div>
   );
